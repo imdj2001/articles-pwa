7ebd8e82df35af4cb10f247feb19d69acf135866
Dhananjay <sdhananjay006@gmail.com>
2021-03-18 10:24:03 +0530

    Click here animation added

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -10,22 +10,22 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import "bootstrap/dist/css/bootstrap.css";
-import './articles.css'
+import "./articles.css";
 const useStyles = makeStyles({
-    root: {
-      maxWidth: 700,
-    },
-    media: {
-      height: 200,
-    },
-  });
+  root: {
+    maxWidth: 700,
+  },
+  media: {
+    height: 200,
+  },
+});
 function Articles() {
   const classes = useStyles();
-  const [lang, setLang] = useState('en');
+  const [lang, setLang] = useState("en");
   const [articles, setArticles] = useState([]);
   const fetchData = async () => {
     const response = await axios.get(
-      `https://gnews.io/api/v4/top-headlines?lang=${lang}&token=6fe76c3950a7f65897cb640c1fe4c265`
+      `https://gnews.io/api/v4/top-headlines?lang=${lang}&token=05cda80d88a76ff542f03a539e7fc4ee`
     );
     setArticles(response.data.articles);
   };
@@ -39,50 +39,80 @@ function Articles() {
 
   return (
     <>
-      <Navbar bg="dark" variant="dark" style={{display:"flex" , justifyContent:"space-between"}}>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
         <Navbar.Brand href="#home">ARTICLES-PWA</Navbar.Brand>
-          <NavDropdown title="Languages" className="drpdwn">
-        <NavDropdown.Item onClick={() => language("en")} >English</NavDropdown.Item>
-        <NavDropdown.Item onClick={() => language("hi")} >हिन्दी</NavDropdown.Item>
-        <NavDropdown.Item onClick={() => language("te")} >తెలుగు</NavDropdown.Item>
-        <NavDropdown.Item onClick={() => language("ta")} >தமிழ்</NavDropdown.Item>
-        <NavDropdown.Item onClick={() => language("mr")} >मराठी</NavDropdown.Item>
-      </NavDropdown>
-      
+        <NavDropdown title="Languages" className="drpdwn">
+          <NavDropdown.Item onClick={() => language("en")}>
+            English
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => language("hi")}>
+            हिन्दी
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => language("te")}>
+            తెలుగు
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => language("ta")}>
+            தமிழ்
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => language("mr")}>
+            मराठी
+          </NavDropdown.Item>
+        </NavDropdown>
       </Navbar>
       <div className="container__art">
-        <div style={{height:"20px"}}></div>
+        <div style={{ height: "20px" }}></div>
         {articles.length &&
           articles.map((art) => {
-               console.log(art)
+            console.log(art);
             return (
-              <div className="d-flex justify-content-center"> 
-                <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={art.image ? art.image : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Google_News_icon.svg/1200px-Google_News_icon.svg.png"}
-          title="news"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-             {art.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-           {art.content}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        {/* <Button size="small" color="primary">
-          Share
-        </Button> */}
-        <Button size="small" color="primary" href={art.url}>
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-    <div style={{height:"15px"}}></div>
+              <div className="d-flex justify-content-center">
+               <Card className={classes.root}>
+               <a href="https://play.google.com/store/apps/details?id=com.whatsapp" >
+               <img  className="click__here" href="https://play.google.com/store/apps/details?id=com.whatsapp" src="https://media0.giphy.com/media/2ZWntY8HkszU45sUJC/giphy.gif"/>
+               </a>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      image={
+                        art.image
+                          ? art.image
+                          : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Google_News_icon.svg/1200px-Google_News_icon.svg.png"
+                      }
+                      title="news"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {art.title}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {art.content}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                        <Button size="small" style={{height:"10px"}} href="https://play.google.com/store/apps/details?id=com.whatsapp" >
+                            <img style={{height:"10%"}}src="https://www.animatedimages.org/data/media/1096/animated-click-here-sign-and-button-image-0009.gif"/>
+                        </Button>
+                    <Button size="small" color="primary" href={art.url}>
+                      Go for Full News
+                    </Button>
+                  </CardActions>
+                 <a href="https://play.google.com/store/apps/details?id=com.whatsapp" >
+                 <img className="click__here" src="http://cdn.lowgif.com/full/441bb54b885f7a06-click-here-icon-bethel-bible-college-guntur.gif" />
+                   </a> 
+                  
+ 
+                </Card>
+               
+                
               </div>
             );
           })}
